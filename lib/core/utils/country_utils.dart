8206a4c011f31c335cbd41a7514626ccc379ea82
DAMMAK <adekoyafelix@gmail.com>
2025-03-01 11:44:23 +0100

    updates on the utils file

--- lib/core/utils/country_utils.dart
+++ lib/core/utils/country_utils.dart
@@ -14,4 +14,23 @@ class CountryUtils {
 
     return capitals.join(', ');
   }
+
+  static String getCountryCode(Map<String, dynamic>? idd) {
+    if (idd == null || idd.isEmpty) {
+      return '';
+    }
+
+    return (idd["suffixes"] as List)
+        .map<String>((suffix) => (idd["root"]) + suffix)
+        .toList()
+        .join(",");
+  }
+
+  static String getCurrencyString(Map<String, dynamic>? currencies) {
+    if (currencies == null || currencies.isEmpty) {
+      return '';
+    }
+
+    return currencies.values.map<String>((cur) => cur["name"]).join(", ");
+  }
 }
--- lib/features/countries/data/models/country_detail_model.dart
+++ lib/features/countries/data/models/country_detail_model.dart
@@ -1,3 +1,4 @@
+import 'package:cubex_demo/core/utils/country_utils.dart';
 import 'package:cubex_demo/features/countries/domain/entities/country_detail.dart';
 
 class CountryDetailModel extends CountryDetail {
@@ -41,10 +42,10 @@ class CountryDetailModel extends CountryDetail {
 
   factory CountryDetailModel.fromJson(Map<String, dynamic> json) {
     final name = json['name']['common'] as String;
-    final capital = json['capital'][0] as String;
+    final capital = CountryUtils.getCapitalString(json['capital']);
     final flag = json['flags']['png'] as String;
     final area = json["area"];
-    final languages = (json['languages'] as Map).values.toList().join(",");
+    final languages = CountryUtils.getLanguagesString(json['languages']);
     final population = json['population'];
     final continent = json['continents'][0] as String;
     final timezones = json['timezones'][0] as String;
@@ -53,18 +54,10 @@ class CountryDetailModel extends CountryDetail {
     final startOfWeek = json['startOfWeek'] as String;
     final capitalCoordinate = json['latlng'] as List;
     final subRegion = json['subregion'] as String;
-    final countryCode = (json['idd']["suffixes"] as List)
-        .map<String>((suffix) => (json['idd']["root"]) + suffix)
-        .toList()
-        .join(",");
+    final countryCode = CountryUtils.getCountryCode(json['idd']);
     final independent = json['independent'].toString();
     final unMember = json['unMember'].toString();
-    final currency = (json['currencies'] as Map)
-        .values
-        .map<String>((cur) => cur["name"])
-        .toList()
-        .join(",");
-    print(currency);
+    final currency = CountryUtils.getCurrencyString(json['currencies']);
 
     return CountryDetailModel(
       name: name,
